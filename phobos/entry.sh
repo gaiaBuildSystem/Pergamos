8c08d2f09da0e2adae24f3812777e5d66a645171
Matheus Castello <matheus@castello.eng.br>
2025-08-14 09:15:11 -0300

    phobos: Updates for qemuarm64 support
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- phobos/entry.sh
+++ phobos/entry.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -e
+
+_IMAGE_VERSION="0.0.6"
+
 if [ "$1" == "dhcp" ]; then
     echo "Starting DHCP server ..."
 
@@ -9,9 +13,71 @@ if [ "$1" == "dhcp" ]; then
     exit 0
 fi
 
+# for the virtual TPM v2
+if [ "$1" == "tpm2" ]; then
+    echo "Starting virtual TPM v2 ..."
+
+    # Clean state
+    rm -rf /tmp/mytpm1
+    rm -rf /tmp/pemsafe
+    mkdir -p /tmp/pemsafe
+
+    swtpm socket --tpmstate dir=/tmp/pemsafe \
+        --ctrl type=unixio,path=/tmp/pemsafe/swtpm-sock \
+        --tpm2 \
+        --log level=20
+
+    exit 0
+fi
+
+# to download the image from the magalu objects
+if [ "$1" == "image-download" ]; then
+    # check if the /host/phobos.img file
+    # if not we need to download it
+    # and extract it
+    if [ ! -f /host/phobos-${_IMAGE_VERSION}.img ]; then
+        echo "phobos.img file not found!"
+        echo "Downloading it ..."
+
+        IMAGE_ARCH=$(arch)
+        MACHINE=""
+
+        if [ "${IMAGE_ARCH}" = "x86_64" ]; then
+            MACHINE="qemux86-64"; \
+        elif [ "${IMAGE_ARCH}" = "aarch64" ]; then
+            MACHINE="qemuarm64"; \
+        else
+            echo "Unsupported architecture: ${IMAGE_ARCH}";
+            exit 69
+        fi
+
+        curl -L -o img.tar.xz \
+            https://br-se1.magaluobjects.com/gaia-imgs/${MACHINE}-ota-0-0-0.img.tar.xz
+        tar -xf img.tar.xz
+        rm img.tar.xz
+        mv ${MACHINE}-ota-0-0-0.img /host/phobos.img
+
+        # for arm64 we will need also the bios
+        if [ "${IMAGE_ARCH}" = "aarch64" ]; then
+            curl -L -o u-boot.bin \
+                https://br-se1.magaluobjects.com/gaia-imgs/u-boot.bin
+
+            mv u-boot.bin /host/u-boot.bin
+        fi
+
+        # lock file for the version ok
+        touch /host/phobos-${_IMAGE_VERSION}.img
+    fi
+
+    exit 0
+fi
+
+
 chmod +s /usr/lib/qemu/qemu-bridge-helper
 mkdir /etc/qemu
 echo "allow docker0" > /etc/qemu/bridge.conf
+cp /usr/share/OVMF/OVMF_VARS_4M.fd /tmp/OVMF_VARS.fd
+
 
 _hdSize=$STORAGE
 _ramSize=$(printf "%.0f" $RAM)
@@ -29,14 +95,14 @@ fi
 
 # check if it comes from a user vm named
 if [ -z "$_name" ] || [ "$_name" = '""' ] || [ "$_name" = "None" ]; then
-    _name="/phobos.img"
+    _name="/host/phobos.img"
 else
     _name="/root/.pem/phobos-$USER_VM_NAME.img"
 
     # check if the file exists
     if [ ! -f "$_name" ]; then
         echo "File $_name not found, first time running it ..."
-        cp /phobos.img $_name
+        cp /host/phobos.img $_name
     fi
 
     echo "Using $_name as the image file"
@@ -50,11 +116,11 @@ _CPU=host
 
 if [ "$_ARCH" == "x86_64" ]; then
     _QEMU_CMD=qemu-system-x86_64
-    _MACHINE=pc
+    _MACHINE="q35,smm=on"
     _CPU=host
 elif [ "$_ARCH" == "aarch64" ]; then
     _QEMU_CMD=qemu-system-aarch64
-    _MACHINE="virt,highmem=off"
+    _MACHINE="virt,gic-version=3,highmem=on,highmem-redists=on"
     _CPU=host
 else
     echo "Unsupported architecture: $_ARCH"
@@ -64,7 +130,7 @@ fi
 if [ $_instances -gt 1 ]; then
     for i in $(seq 1 $_instances); do
         _random_mac=$(printf 'DE:AD:BE:EF:%02X:%02X' $((RANDOM%256)) $((RANDOM%256)))
-        cp /phobos.img /phobos$i.img
+        cp /host/phobos.img /phobos$i.img
 
         qemu-img resize -f raw /phobos$i.img +${_hdSize}G
 
@@ -101,7 +167,8 @@ qemu-img resize -f raw $_name +${_hdSize}G
 
 $_QEMU_CMD \
     -name "PhobOS Emulator" \
-    -cpu host \
+    $(if [ "$NO_KVM" != "1" ]; then echo "-cpu host"; fi) \
+    $(if [ "$NO_KVM" == "1" ]; then echo "-cpu max"; fi) \
     -smp 4 \
     --netdev bridge,id=hn0,br=docker0 \
     -device virtio-net-pci,netdev=hn0,id=nic1,mac=$_random_mac \
@@ -111,7 +178,13 @@ $_QEMU_CMD \
     -device virtio-tablet-pci \
     -display gtk,zoom-to-fit=off \
     -m $_ramSize \
-    -drive file=$_name,format=raw \
-    -bios /usr/share/ovmf/OVMF.fd \
+    -drive file=$_name,format=raw,if=virtio \
+    $(if [ "$_ARCH" == "x86_64" ]; then echo "-drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE_4M.fd"; fi) \
+    $(if [ "$_ARCH" == "x86_64" ]; then echo "-drive if=pflash,format=raw,file=/tmp/OVMF_VARS.fd"; fi) \
+    $(if [ "$_ARCH" == "aarch64" ]; then echo "-bios /host/u-boot.bin"; fi) \
+    -chardev socket,id=chrtpm,path=/tmp/pemsafe/swtpm-sock \
+    -tpmdev emulator,id=tpm0,chardev=chrtpm \
+    $(if [ "$_ARCH" == "x86_64" ]; then echo "-device tpm-tis,tpmdev=tpm0"; fi) \
+    $(if [ "$_ARCH" == "aarch64" ]; then echo "-device tpm-tis-device,tpmdev=tpm0"; fi) \
     -serial mon:stdio \
     $(if [ "$NO_KVM" != "1" ]; then echo "-enable-kvm"; fi)
